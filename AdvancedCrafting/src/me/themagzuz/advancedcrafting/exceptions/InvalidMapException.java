ad354b8adc2e441222c6568e849f6bb9ed466737
TheMagzuz <markus.dam123@gmail.com>
2016-04-03 21:05:48 +0200

    Created InvalidMapException

---
+++ AdvancedCrafting/src/me/themagzuz/advancedcrafting/exceptions/InvalidMapException.java
@@ -0,0 +1,18 @@
+package me.themagzuz.advancedcrafting.exceptions;
+
+public class InvalidMapException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2375739685180890122L;
+
+	public InvalidMapException(){
+		super();
+	}
+	
+	public InvalidMapException(String msg){
+		super(msg);
+	}
+	
+}
